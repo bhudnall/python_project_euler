51b26403a9e9f8c2b3108bfd54819dfa7d5f5fdd
bhudnall <brian.hudnall@gmail.com>
2016-01-21 09:26:07 -0800

    finished dict

--- Problem17.py
+++ Problem17.py
@@ -35,21 +35,28 @@ for ten_key, ten_val in tens_sorted:
 
 ##print sum"""
 
+## only need one dict
+
+def convert(num):
+    convert_dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
+          10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen",
+          17: "seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",
+          60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety", 100: "hundred"}
+    return convert_dict[num]
 
 
 sum = 0
-for i in range(1, 5):
-    if i < 10:
-        print get_ten(str(i))
+for i in range(1, 30 + 1):
+    if i < 20:
+        sum += len(convert(i))
+    elif i < 100:
+        print convert(i/10)
+        ##sum += len(convert)
 
-def get_ten(num):
-    through_twenty = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine"}
-    print num
-    return through_twenty[num]
+
+print "sum", sum
+
 
 ## remember to divide by 10, 100, and 1000
 ## get:
-
-
-
-print sum
+## come up with logic send josh pseudocode
